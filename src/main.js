d96571e51b8d0456e69a271d6d3b0b08430b4419
Brian Hensley and Marc Davies <marc@>
2019-06-06 10:19:09 -0700

    attempt to separate UI and backend logic

--- src/main.js
+++ src/main.js
@@ -1,26 +1,42 @@
-import { WeatherService } from './weather-service.js';
-import { body } from 'body-parser';
+// import { WeatherService } from './weather-service.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//
+//     let weatherService = new WeatherService();
+//     let promise = weatherService.getWeatherByCity(city);
+//
+//     promise.then(function(response) {
+//       let body = JSON.parse(response);
+//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+//     });
+//   });
+//
+// });
+
+let WeatherService = require('./weather-service.js').weatherService;
+
+export let displayData = function(response) {
+  let body = JSON.parse(response);
+  $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+  $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+};
+
+
 $(document).ready(function() {
+  let weatherService = new WeatherService();
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
-    $('#location').val("");
-
-
-    let weatherService = new WeatherService();
-    let promise = weatherService.getWeatherByCity(city);
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    // $('#location').val("");
+    weatherService.getWeatherByCity(city, displayData);
   });
-
 });
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,17 +1,47 @@
-export class WeatherService {
-  getWeatherByCity(city) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+// export class WeatherService {
+//   getWeatherByCity(city) {
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API-KEY-GOES-HERE`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.");
+//       }
+//     });
+//   }
+// }
+
+// let WeatherService = function() {}
+// WeatherService.prototype.getWeatherByCity = function(city) {
+//   $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API-KEY-GOES-HERE`).then(function(response) {
+//     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//   }).fail(function(error) {
+//     $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//   });
+// }
+// exports.weatherService = WeatherService;
+
+import { displayData } from './main.js';
+
+var WeatherService = function() {
+
 }
+
+WeatherService.prototype.getWeatherByCity = function(city, displayData) {
+  $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API-KEY-GOES-HERE`)
+    .then(function(results) {
+      displayData(results);
+    })
+    .fail(function() {
+      console.log('something went wrong');
+    });
+}
+
+exports.weatherService = WeatherService;
